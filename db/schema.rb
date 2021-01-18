b26012cd637bbe9363e0903aadbd2b5114068fa5
paymaan73 <paymankazemipymkz@gmail.com>
2021-01-18 17:48:13 +0330

    Create model sport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_140017) do
+ActiveRecord::Schema.define(version: 2021_01_18_141737) do
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
