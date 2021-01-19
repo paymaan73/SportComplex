a679078930aae74328e16b6bfce419e0cccc3dde
paymaan73 <paymankazemipymkz@gmail.com>
2021-01-19 09:18:12 +0330

    routes nampespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
 
   root to: "sports#index"
 
-  resources :users
+  resources :users do
 
-  resources :sports, only:[:index, :create]
+    resources :sports, only:[:index, :create]
+  end
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
 
   post "sport_activity", to: "sports#create"
+
+  get '/users/#misssing', to: "sports#index"
 
 
 end
